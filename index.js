a48a33306df2622bf064949ad71b0e8893506a3a
unknown <tasnimtanzim55@gmail.com>
2022-05-01 01:14:12 +0600

    Single data load by ID added

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const res = require('express/lib/response');
 const app = express();
 require('dotenv').config();
 const port = process.env.PORT || 5000;
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 
 app.use(cors());
 app.use(express.json());
@@ -27,6 +27,12 @@ async function run () {
             const query = {};
             const cursor = carCollection.find(query);
             const result = await cursor.toArray();
+            res.send(result);
+        })
+        app.get('/cars/:id', async (req,res)=>{
+            const id = req.params.id;
+            const query = {_id: ObjectId(id)}
+            const result = await carCollection.findOne(query);
             res.send(result);
         })
     }
