10b954fd0700e19d83306bf70130152f5d1fcaa3
unknown <tasnimtanzim55@gmail.com>
2022-05-01 01:09:25 +0600

    connected with mongodb

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const res = require('express/lib/response');
 const app = express();
 require('dotenv').config();
 const port = process.env.PORT || 5000;
+const { MongoClient, ServerApiVersion } = require('mongodb');
 
 app.use(cors());
 app.use(express.json());
@@ -11,6 +12,30 @@ app.use(express.json());
 app.get('/', (req,res) => {
     res.send("Cars are coming");
 })
+
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.hcyv7.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+async function run () {
+    try{
+        await client.connect();
+        const carCollection = client.db('warehouseManagement').collection('cars');
+
+        app.get('/cars', async (req,res)=>{
+            const query = {};
+            const cursor = carCollection.find(query);
+            const result = await cursor.toArray();
+            res.send(result);
+        })
+    }
+    finally{
+
+    }
+}
+run().catch(console.dir);
+
 
 app.listen(port, ()=>{
     console.log("Listening to port", port);
